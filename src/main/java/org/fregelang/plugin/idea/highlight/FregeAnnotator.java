7a74c09ce44df8fc0891ac61f009190763d94830
Paweł Prażak <pawel.prazak@bluecatcode.com>
2016-03-02 11:47:49 +0100

    Add empty annotator
    - unify with haskell-idea-plugin

---
+++ src/main/java/org/fregelang/plugin/idea/highlight/FregeAnnotator.java
@@ -0,0 +1,14 @@
+package org.fregelang.plugin.idea.highlight;
+
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.TokenSet;
+import org.jetbrains.annotations.NotNull;
+
+public class FregeAnnotator implements Annotator {
+    @Override
+    public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
+        // TODO
+    }
+}
